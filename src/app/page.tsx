23dd34ae7fa05a093e8ef493d982272c523879ac
Wan Sim <0420syj@naver.com>
2023-07-12 21:56:52 +0900

    favicon 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
 	useEffect(() => {
 		console.log('date: ' + getDate());
-	}, [date]);
+	}, [date, getDate]);
 
 	const handleSubmit = (event: {
 		preventDefault: () => void;
