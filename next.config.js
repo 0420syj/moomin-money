9f2135cc6d1593c12af94d62d0e41e7bd0154f89
Wanny.Sim <0420syj@naver.com>
2023-08-03 15:11:47 +0900

    fix: 로그인버튼 프로필 이미지 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+        minimumCacheTTL: 31536000,
+        formats: ['image/avif', 'image/webp'],
+    },
+};
 
 module.exports = nextConfig;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ const Header = () => {
 		process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_LINK ?? ('#' as string);
 
 	return (
-		<header className="flex justify-between w-full p-5 text-white bg-blue-500">
-			<nav>
+		<header className="flex items-center justify-between w-full p-5 text-white bg-blue-500">
+			<nav className="flex items-center h-8">
 				<ul className="flex space-x-4">
 					<li>
 						<a href="/" className="hover:underline">
@@ -17,24 +17,6 @@ const Header = () => {
 					<li>
 						<a href="/moneybook" className="hover:underline">
 							이번달 소비
-						</a>
-					</li>
-					<li>
-						<a
-							href={googleSpreadSheetLink}
-							target="_blank"
-							className="hover:underline"
-						>
-							가계부 링크
-						</a>
-					</li>
-					<li>
-						<a
-							href="https://github.com/0420syj/moomin-money/issues/28"
-							target="_blank"
-							className="hover:underline"
-						>
-							개선요청
 						</a>
 					</li>
 				</ul>
