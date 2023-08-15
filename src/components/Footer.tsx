6b3b053bfc5a277c814447b029c903344e551f35
Wanny.Sim <0420syj@naver.com>
2023-08-16 00:49:58 +0900

    fix: a 태그 삭제

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,34 @@
+import Link from 'next/link';
+
 const Footer = () => {
 	const googleSpreadSheetLink =
 		process.env.NEXT_PUBLIC_GOOGLE_SPREADSHEET_LINK ?? ('#' as string);
 
 	return (
 		<footer className="w-full p-4 mt-4 text-center bg-gray-200">
-			<p>
-				<a href="/" className="hover:underline">
-					홈
-				</a>{' '}
-				|{' '}
-				<a href="/moneybook" className="hover:underline">
-					최근 2개월 소비
-				</a>{' '}
-				|{' '}
-				<a
-					href={googleSpreadSheetLink}
-					target="_blank"
-					className="hover:underline"
-				>
-					가계부 링크
-				</a>
-				|{' '}
-				<a
-					href="https://github.com/0420syj/moomin-money/issues/28"
-					target="_blank"
-					className="hover:underline"
-				>
-					개선요청
-				</a>
-			</p>
+			<Link href="/" className="hover:underline">
+				홈
+			</Link>{' '}
+			|{' '}
+			<Link href="/moneybook" className="hover:underline">
+				최근 2개월 소비
+			</Link>{' '}
+			|{' '}
+			<Link
+				href={googleSpreadSheetLink}
+				target="_blank"
+				className="hover:underline"
+			>
+				가계부 링크
+			</Link>
+			|{' '}
+			<Link
+				href="https://github.com/0420syj/moomin-money/issues/28"
+				target="_blank"
+				className="hover:underline"
+			>
+				개선요청
+			</Link>
 		</footer>
 	);
 };
