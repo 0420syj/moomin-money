cb1aed035228904830bab7f5e70714cf652f01e8
Wan Sim <0420syj@naver.com>
2023-10-16 00:04:13 +0900

    feat: 금액 input 계산기능 추가

--- src/components/PriceInput.tsx
+++ src/components/PriceInput.tsx
@@ -1,30 +1,40 @@
-import Calculator from '@/components/Calculator/Calculator';
 import useFormStore from '@/hooks/useFormStore';
 import { useState } from 'react';
 
 const PriceInput: React.FC = () => {
-	const [openCalculator, setOpenCalculator] = useState(false);
-
+	const [priceString, setPriceString] = useState<string>('');
 	const { price, setPrice } = useFormStore(state => ({
 		price: state.price,
 		setPrice: state.actions.setPrice,
 	}));
 
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const priceValue = e.target.value.replace(/[^0-9.-]/g, '');
+	const formatNumberWithComma = (num: number): string => {
+		return num.toLocaleString('en-US');
+	};
 
-		setPrice(Number(priceValue));
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPriceString(e.target.value);
 	};
 
 	const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
 		let priceValue = e.target.value;
 
-		if (priceValue === '-' || priceValue === '.' || priceValue === '-.') {
+		try {
+			const calculatedValue = eval(priceValue.replace(/,/g, ''));
+			setPriceString(formatNumberWithComma(calculatedValue));
+			setPrice(calculatedValue);
+		} catch (error) {
 			setPrice(0);
+			setPriceString('0');
+		}
+	};
+
+	const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+		const priceValue = priceString.replace(/,/g, '');
+		if (priceValue === '0') {
+			setPriceString('');
 		} else {
-			// remove , from priceValue
-			priceValue = priceValue.replace(/,/g, '');
-			setPrice(Number(priceValue));
+			setPriceString(priceValue);
 		}
 	};
 
@@ -40,22 +50,14 @@ const PriceInput: React.FC = () => {
 				required
 				id="price"
 				type="text"
-				value={price === 0 ? '' : price.toLocaleString()}
+				value={priceString}
 				onChange={handleChange}
 				onBlur={handleBlur}
+				onFocus={handleFocus}
 				autoComplete="off"
 				inputMode="numeric"
 				className="block w-full px-4 py-2 mt-1 border-gray-300 rounded-lg shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
 			/>
-			{/* TODO: 계산기 구현중 */}
-			{/* <button
-				type="button"
-				onClick={() => setOpenCalculator(!openCalculator)}
-				className="px-4 py-2 mt-1 text-sm font-medium text-white bg-blue-600 rounded-lg shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-			>
-				계산기
-			</button>
-			{openCalculator && <Calculator />} */}
 		</>
 	);
 };
